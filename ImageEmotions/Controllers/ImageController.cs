03c543d8157ab3a6d29f41e770e17dd3f0bcb0d6
Jesse <jesse.holwell@au.ey.com>
2017-11-16 12:42:23 +1100

    project init
    fake api post and response

---
+++ ImageEmotions/Controllers/ImageController.cs
@@ -0,0 +1,77 @@
+﻿using ImageEmotions.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageEmotions.Controllers
+{
+    public class ImageController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            //TODO: convert HttpPostedFileBase into a octet stream
+
+            Task.Run(() => IndexAsync());
+
+            var results = @"[{'faceRectangle':{
+                'height': 89,
+                'left': 95,
+                'top': 54,
+                'width': 89
+            },
+            'scores': {
+                'anger': 7.96481145e-11,
+                'contempt': 8.144482e-13,
+                'disgust': 4.03720182e-11,
+                'fear': 2.92296515e-13,
+                'happiness': 1,
+                'neutral': 1.95017071e-12,
+                'sadness': 1.07769696e-13,
+                'surprise': 7.470441e-11
+            }]";
+
+
+            EmotionsViewModel vm = new EmotionsViewModel(results);
+
+            return View("Results", vm);
+        }
+
+        public async Task IndexAsync()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "8054ac830fda40b7a531c8e310ccd0fc");
+
+            var uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+            }
+        }
+    }
+}
---
+++ ImageEmotions/Models/EmotionsViewModel.cs
@@ -0,0 +1,71 @@
+﻿namespace ImageEmotions.Models
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Web;
+    using Newtonsoft.Json.Linq;
+    using System;
+
+    public class EmotionsViewModel
+    {
+        public EmotionsViewModel(string Json)
+        {
+            //TODO: stuff
+            //Parse Json into Emotions
+
+            JObject joResponse = JObject.Parse(Json);
+            JObject ojObject = (JObject)joResponse["response"];
+            JArray array = (JArray)ojObject["chats"];
+            int id = Convert.ToInt32(array[0].ToString());
+
+            Emotions = new List<EmotionsModel>();
+            EmotionsModel model = new EmotionsModel();
+
+            model.Scores = new List<EmotionScore>();
+            EmotionScore emo = new EmotionScore();
+            emo.key = "anger";
+            emo.value = 70;
+
+            model.Scores.Add(emo);
+            model.Face = new ImageFaceModel();
+
+            Emotions.Add(model);
+        }
+
+        public ICollection<EmotionsModel> Emotions { get; set; }
+
+        public Image Image { get; set; }
+    }
+
+    public class EmotionsModel
+    {
+        public EmotionsModel()
+        {
+            Face = new ImageFaceModel();
+            Scores = new List<EmotionScore>();
+        }
+
+        public ImageFaceModel Face { get; set; }
+        public List<EmotionScore> Scores { get; set; }
+    }
+
+    public class ImageFaceModel
+    {
+        public ImageFaceModel()
+        {
+            Face = new Rectangle();
+            Image = @"http://4vector.com/i/free-vector-rubik-s-cube-random-clip-art_106251_Rubiks_Cube_Random_clip_art_medium.png";
+        }
+
+        public Rectangle Face { get; set; }
+
+        public string Image { get; set; }
+
+    }
+
+    public class EmotionScore
+    {
+        public string key { get; set; }
+        public float value { get; set; }
+    }
+}
