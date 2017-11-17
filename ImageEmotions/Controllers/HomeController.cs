9e37a8a6128c93fc28db9f9b95b58c3e15d59296
Jesse <jesse.holwell@au.ey.com>
2017-11-17 16:39:37 +1100

    first release

--- ImageEmotions/Controllers/HomeController.cs
+++ ImageEmotions/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using ImageEmotions.Models;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +10,74 @@ namespace ImageEmotions.Controllers
 {
     public class HomeController : Controller
     {
+        public static readonly HttpClient client = new HttpClient();
+        private HttpResponseMessage myResponse;
+        private static string uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize";
+        private string results;
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public async Task<ActionResult> Index(HttpPostedFileBase file, string url)
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+            string results = "";
+
+            //try
+            //{
+            //if (file != null)
+            //    await Task.Run(() => ProcessImageAsync(file));
+            //else
+            //await Task.Run(() => ProcessUrlAsync(url));
+
+            results = await Task.Run(() => ProcessImageAsync(file));
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    return View("Error", ex.Message as object);
+            //}
+
+            EmotionsViewModel vm = new EmotionsViewModel(results);
+
+            return View("Results", vm);
+
         }
 
-        public ActionResult Contact()
+        public async Task<string> ProcessImageAsync(HttpPostedFileBase file)
         {
-            ViewBag.Message = "Your contact page.";
+            //convert HttpPostedFileBase into a octet stream
+            BinaryReader b = new BinaryReader(file.InputStream);
+            byte[] byteData = b.ReadBytes(file.ContentLength);
 
-            return View();
+            //process byte data
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "e39efb06499b4fa988f354e3d43e7834");
+                    using (var myResponse = await client.PostAsync(uri, content))
+                    {
+                        var results = await myResponse.Content.ReadAsStringAsync();
+                        return results;
+                    }
+                }
+            }
         }
+
+        //public async Task ProcessUrlAsync(string url)
+        //{
+        //    //process url
+        //    var content = new StringContent(@"{'url': 'https://res.cloudinary.com/demo/image/upload/w_400/woman.jpg' }");
+        //    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        //    myResponse = await client.PostAsync(uri, content);
+        //    results = await myResponse.Content.ReadAsStringAsync();
+
+        //}
     }
 }
--- ImageEmotions/Models/EmotionsViewModel.cs
+++ ImageEmotions/Models/EmotionsViewModel.cs
@@ -5,34 +5,50 @@
     using System.Web;
     using Newtonsoft.Json.Linq;
     using System;
+    using Newtonsoft.Json;
+    using System.Collections;
+    using System.Collections.ObjectModel;
+    using System.Linq;
 
     public class EmotionsViewModel
     {
         public EmotionsViewModel(string Json)
         {
-            //TODO: stuff
-            //Parse Json into Emotions
-
-            JObject joResponse = JObject.Parse(Json);
-            JObject ojObject = (JObject)joResponse["response"];
-            JArray array = (JArray)ojObject["chats"];
-            int id = Convert.ToInt32(array[0].ToString());
-
-            Emotions = new List<EmotionsModel>();
             EmotionsModel model = new EmotionsModel();
 
-            model.Scores = new List<EmotionScore>();
-            EmotionScore emo = new EmotionScore();
-            emo.key = "anger";
-            emo.value = 70;
+            try
+            {
 
-            model.Scores.Add(emo);
-            model.Face = new ImageFaceModel();
+                RootObject[] obj = JsonConvert.DeserializeObject<RootObject[]>(Json);
 
-            Emotions.Add(model);
+                //foreach (var emotion in obj)
+                var emotion = obj.FirstOrDefault();
+
+                model.Scores = new List<EmotionScore>()
+                    {
+                        new EmotionScore("anger", emotion.scores.anger*100),
+                        new EmotionScore("contempt", emotion.scores.contempt*100),
+                        new EmotionScore("disgust", emotion.scores.disgust*100),
+                        new EmotionScore("fear", emotion.scores.fear*100),
+                        new EmotionScore("happiness", emotion.scores.happiness*100),
+                        new EmotionScore("neutral", emotion.scores.neutral*100),
+                        new EmotionScore("sadness", emotion.scores.sadness*100),
+                        new EmotionScore("surprise", emotion.scores.surprise*100)
+                    };
+                model.Face = new Rectangle(emotion.faceRectangle.left, emotion.faceRectangle.top,
+                    emotion.faceRectangle.width, emotion.faceRectangle.height);
+
+            }
+            catch (Exception ex)
+            {
+                //System.Diagnostics.Debugger.Break();
+                Console.WriteLine(ex.Message);
+            }
+
+            this.Emotions = model;
         }
 
-        public ICollection<EmotionsModel> Emotions { get; set; }
+        public EmotionsModel Emotions { get; set; }
 
         public Image Image { get; set; }
     }
@@ -41,31 +57,52 @@
     {
         public EmotionsModel()
         {
-            Face = new ImageFaceModel();
+            Face = new Rectangle();
             Scores = new List<EmotionScore>();
         }
 
-        public ImageFaceModel Face { get; set; }
-        public List<EmotionScore> Scores { get; set; }
-    }
-
-    public class ImageFaceModel
-    {
-        public ImageFaceModel()
-        {
-            Face = new Rectangle();
-            Image = @"http://4vector.com/i/free-vector-rubik-s-cube-random-clip-art_106251_Rubiks_Cube_Random_clip_art_medium.png";
-        }
-
         public Rectangle Face { get; set; }
-
-        public string Image { get; set; }
-
+        public List<EmotionScore> Scores { get; set; }
     }
 
     public class EmotionScore
     {
+        public EmotionScore(string Key, double Value)
+        {
+            this.key = Key;
+            this.value = Value;
+        }
+
         public string key { get; set; }
-        public float value { get; set; }
+        public double value { get; set; }
+    }
+
+
+
+    //Generated
+    public class FaceRectangle
+    {
+        public int height { get; set; }
+        public int left { get; set; }
+        public int top { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Scores
+    {
+        public double anger { get; set; }
+        public double contempt { get; set; }
+        public double disgust { get; set; }
+        public double fear { get; set; }
+        public double happiness { get; set; }
+        public double neutral { get; set; }
+        public double sadness { get; set; }
+        public double surprise { get; set; }
+    }
+
+    public class RootObject
+    {
+        public FaceRectangle faceRectangle { get; set; }
+        public Scores scores { get; set; }
     }
 }
