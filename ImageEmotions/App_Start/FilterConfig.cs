d56f7718de02a1e174605137e4bb6310ca1d2f8f
Jesse Holwell <jesse.d.holwell@gmail.com>
2017-11-20 19:26:01 +1100

    app insights

--- ImageEmotions/App_Start/FilterConfig.cs
+++ ImageEmotions/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace ImageEmotions
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
