72082fa6856fff5aba4aa9c0bd177933acb2dc85
Jesse <jesse.holwell@au.ey.com>
2018-03-29 16:39:31 +1100

    implement webcam streaming and snapshots attempting to implement new key and continuous submission

--- ImageEmotions/Controllers/HomeController.cs
+++ ImageEmotions/Controllers/HomeController.cs
@@ -12,17 +12,21 @@ namespace ImageEmotions.Controllers
     public class HomeController : Controller
     {
         public static readonly HttpClient client = new HttpClient();
-        private static string uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize";
+        private static string uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize/";
 
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Index(HttpPostedFileBase file, string url)
+        public ActionResult Stream()
         {
+            return View();
+        }
 
+        [HttpPost]
+        public async Task<ActionResult> Submit(HttpPostedFileBase file, string url)
+        {
             string results = "";
 
             try
@@ -47,20 +51,48 @@ namespace ImageEmotions.Controllers
 
         }
 
+        [HttpPost]
+        public async Task<ActionResult> SubmitFromCanvas(string image)
+        {
+            string results = "";
+
+            string strData = image.Substring("data:image/png;base64,".Length);
+            byte[] data = Convert.FromBase64String(strData);
+
+            try
+            {
+                results = await Task.Run(() => ProcessImageAsync(data));
+            }
+            catch (Exception ex)
+            {
+                return View("Error", ex.Message as object);
+            }
+
+            EmotionsViewModel vm = new EmotionsViewModel(results);
+
+            return Json(new { success = true, result = results });
+
+        }
+
         public async Task<string> ProcessImageAsync(HttpPostedFileBase file)
         {
             //convert HttpPostedFileBase into a octet stream
             BinaryReader b = new BinaryReader(file.InputStream);
             byte[] byteData = b.ReadBytes(file.ContentLength);
 
+            return await ProcessImageAsync(byteData);
+        }
+
+        public async Task<string> ProcessImageAsync(byte[] byteData)
+        {
             //process byte data
             using (var content = new ByteArrayContent(byteData))
             {
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
                 using (var client = new HttpClient())
                 {
-                    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "e36b6495645d4d649f740b912f6efd4c");
+                    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "9d50196588bd4444ad9c23dd452f772c");
                     using (var myResponse = await client.PostAsync(uri, content))
                     {
                         var results = await myResponse.Content.ReadAsStringAsync();
@@ -78,5 +110,37 @@ namespace ImageEmotions.Controllers
         //    myResponse = await client.PostAsync(uri, content);
         //    results = await myResponse.Content.ReadAsStringAsync();
         //}
+
+
+        async void MakeRequest()
+        {
+            //var client = new HttpClient();
+            //var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            //// Request headers
+            //client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{subscription key}");
+
+            //// Request parameters
+            //queryString["outputStyle"] = "aggregate";
+            //var uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognizeinvideo?" + queryString;
+
+            //HttpResponseMessage response;
+
+            // Request body
+            //byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+
+            //using (var content = new ByteArrayContent(byteData))
+            //{
+            //    content.Headers.ContentType = new MediaTypeHeaderValue("< your content type, i.e. application/json >");
+            //    response = await client.PostAsync(uri, content);
+            //}
+
+        }
+
     }
+
+
+
+
+
 }
